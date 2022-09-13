f16258c8e2cff79d588bae69a89e7bd37ff52f74
Modupe91 <feliciaabereola@gmail.com>
2022-09-13 12:29:09 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,8 @@ void print_times_table(int n)
 				}
 				else if (c < 10)
 				{
-					_putchar(' '); _putchar(' ');
+					_putchar(' ');
+				       	_putchar(' ');
 					_putchar('0' + f);
 				}
 				else if (c < 100)
@@ -42,7 +43,8 @@ void print_times_table(int n)
 				}
 				if (b < n)
 				{
-					_putchar(','); _putchar(' ');
+					_putchar(',');
+				       	_putchar(' ');
 				}
 				else
 					_putchar('\n');
